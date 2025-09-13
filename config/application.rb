93da5732df2810e4b66729cdcefb0492db451163
alfonsomartinezdev <alfonso@alfonsoalfonso.dev>
2025-09-13 13:03:07 -0700

    update cors

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module RpgAnnotatorApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:5173"
+        origins "http://localhost:5173", "https://rpg-annotator-frontend.onrender.com"
         resource "*", headers: :any, methods: [ :get, :post, :put, :delete, :options ]
       end
     end
