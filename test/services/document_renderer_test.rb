3ef202f2ea8eb6bd36f1978d5c2bb5370e789a4c
alfonsomartinezdev <alfonso@alfonsoalfonso.dev>
2025-09-17 08:56:10 -0700

    update action doc

--- test/services/document_renderer_test.rb
+++ test/services/document_renderer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class DocumentRendererTest < ActiveSupport::TestCase
   setup do
-    file_path = Rails.root.join("app/data/scp_document.html")
+    file_path = Rails.root.join("app/data/docs/scp_document.html")
     @document = Document.create!(
       title: "Test SCP Document",
       content: File.read(file_path),
@@ -68,4 +68,37 @@ class DocumentRendererTest < ActiveSupport::TestCase
       assert_includes rendered_content, expected_span
     end
   end
+
+  test "wraps annotation text with span MARVEL ACTION" do
+    marvel_file_path = Rails.root.join("app/data/docs/marvel_heroic_action.html")
+    marvel_doc = Document.create!(
+      title: "Test Marvel Action File",
+      content: File.read(marvel_file_path),
+      author: "Anonymous"
+    )
+
+    selection_text = "effect"
+    start_offset = 10272
+    end_offset = 10273
+    puts "test output: ok"
+    puts "#{selection_text}: #{start_offset}-#{end_offset}"
+
+    annotation = marvel_doc.annotations.create!(
+      start_offset: 10272,
+      end_offset: 10278,
+      selection_text: selection_text,
+      annotation_text: "test annotation",
+      author: "Tester"
+    )
+
+    renderer = DocumentRenderer.new(marvel_doc)
+    rendered_content = renderer.render
+
+    expected_span = %(<span class="annotation" data-id="#{annotation.id}">#{selection_text}</span>)
+    assert_includes rendered_content, expected_span
+
+    annotated_fragment = rendered_content.match(/<span class="annotation"[^>]*>(.*?)<\/span>/)[1]
+    assert_equal selection_text, annotated_fragment
+  end
+
 end
